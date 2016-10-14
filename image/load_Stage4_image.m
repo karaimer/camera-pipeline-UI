5d188697e51a402c083728c4efa10131fbcf8550
karaimer <karaimer@users.noreply.github.com>
2016-10-14 11:15:05 +0200

    Update load_Stage4_image.m

--- image/load_Stage4_image.m
+++ image/load_Stage4_image.m
@@ -1,14 +1,6 @@
 close all
 clear all
-
-% r1 = importdata('C:\Users\hakki\Downloads\camera-pipeline-UI-master\camera-pipeline-UI-master\image\r.txt');
-% g1 = importdata('C:\Users\hakki\Downloads\camera-pipeline-UI-master\camera-pipeline-UI-master\image\g.txt');
-% b1 = importdata('C:\Users\hakki\Downloads\camera-pipeline-UI-master\camera-pipeline-UI-master\image\b.txt');
-% [m,n] = size(r1); 
-% image1 = zeros(m,n,3);
-% image1(:,:,1) = r1;
-% image1(:,:,2) = g1;
-% image1(:,:,3) = b1;
+ 
 
 % save image as binary file 
 % fd = fopen('x.txt','w');
@@ -21,9 +13,12 @@ clear all
 % fd3 = fopen('z.txt','w');
 %     fwrite(fd3,image1(:,:,3)','double');
 %     fclose(fd3);
+
+
+
     
 % image = im2double(image)/65536;
-% figure, imshow(image1)
+
 
     fd1 = fopen('r.txt','r');
     fd2 = fopen('g.txt','r');
@@ -44,20 +39,7 @@ image(:,:,3) = b';
 image = im2double(image);
 figure, imshow(image)
 
-% %% proof that txt files and stage 4 size is not equal.
-% % prophoto2 = im2double(imread('..\current_result.tif'));
-% %center aligned
-% eucliudian_error1 = sqrt((image(:,:,1)-...
-%     image1(:,:,1)).^2 + ...
-%     (image(:,:,2)-...
-%     image1(:,:,2)).^2 + ...
-%     (image(:,:,3)-...
-%     image1(:,:,3)).^2);
-% sum(eucliudian_error1(:))
-% figure,
-% imagesc(eucliudian_error1, [0 0.01]);
-% 
-% % abs()
+
     currentfilename = ['stage4.tif'];
     filename = currentfilename;
     outputFileName = filename;
